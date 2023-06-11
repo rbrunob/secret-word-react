e132ec48aaf66a8f43a8352996bc05b8c87c25a1
rbrunob <bruno.bragaramos@gmail.com>
2023-06-11 15:54:21 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+// css
 import './App.css'
 
+// react
+import { useCallback, useEffect, useState } from 'react'
+
+// data
+import { wordList } from './data/words'
+
+// components
+import StartScreen from './components/StartScreen/StartScreen'
+import Game from './components/Game/Game'
+import GameOver from './components/GameOver/GameOver'
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+]
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      {gameStage == 'start' && <StartScreen />}
+      {gameStage == 'game' && <Game />}
+      {gameStage == 'end' && <GameOver />}
+    </div>
   )
 }
 
